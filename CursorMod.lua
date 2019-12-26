8d536e40676831a7bcb1cb55a9b5c81008c330ad
sfmict <sfmict@gmail.com>
2019-12-26 17:42:48 +0300

    remove comments

--- CursorMod.lua
+++ CursorMod.lua
@@ -1,22 +1,17 @@
 local cursor, f = UIParent:CreateTexture(nil, "OVERLAY"), 0
 UIParent.cursorTexture = cursor
 cursor:SetTexture("Interface/AddOns/CursorMod/texture/point.blp")
--- local size = 22.755556106567
--- local resolution = GetCVar("gxMaximize") == "1" and GetCVar("gxFullscreenResolution") or GetCVar("gxWindowedResolution")
--- local resW, resH = resolution:match("^(%d*)x(%d*)")
--- local nsize = size 
--- fprint(nsize)
 
 
 local function show()
 	local x, y = GetCursorPosition()
 	local scale = UIParent:GetEffectiveScale() * cursor:GetScale()
-	-- local sizeScale = nsize / scale
-	-- cursor:SetSize(nsize, nsize)
+
 	cursor:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", x / scale, y / scale)
 	cursor:Show()
 	f = f + 1
 end
+
 
 local function hide(force)
 	f = f - 1
@@ -24,11 +19,6 @@ local function hide(force)
 	if f == 0 then cursor:Hide() end
 end
 
--- if ( IsMouselooking() ) then
--- 	MouselookStop();
--- end
-
--- CVAR cursorsizepreferred
 
 hooksecurefunc("TurnOrActionStart",show)
 hooksecurefunc("TurnOrActionStop",hide)
